0e417a910a44bad0aaecc50e870de36718730a82
ileunam <manuelvaalram@gmail.com>
2019-09-25 17:01:30 -0300

    copyurl and login with thread

--- src/UiComponents/Controllers/LoginViewController.java
+++ src/UiComponents/Controllers/LoginViewController.java
@@ -13,6 +13,9 @@ import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -43,22 +46,62 @@ public class LoginViewController implements Initializable,Notification {
 
     @FXML
     private void copiarUrl(ActionEvent event) throws TwitterException {
-        StringSelection stringSelection = new StringSelection(BOT.getInstance().generateUrl());
-        Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
-        clpbrd.setContents(stringSelection, null);
-        this.newNotification("URL Copiado al portapapeles");
+        
+       new Thread(this::copyUrlThread).start();
+
     }
 
     @FXML
     private void login(ActionEvent event) throws IOException, TwitterException {
-        BOT.getInstance().tryPin(pinBox.getText());
-        if(BOT.getInstance().isAccess()){
-            this.newNotification("Pin Correcto");
-            StackPane newPanel = FXMLLoader.load(getClass().getResource("/UiComponents/Fxml/userView.fxml"));
-            contentPane.getChildren().setAll(newPanel);   
-        }else{
-            this.newNotification("Pin Incorrecto");
+        
+        new Thread(this::loginThread).start();
+        
+    }
+    
+    private void loginThread(){
+        contentPane.setVisible(false);
+        try {
+            BOT.getInstance().tryPin(pinBox.getText());
+            
+            if(BOT.getInstance().isAccess()){
+                
+                Platform.runLater(()->{
+                    
+                    StackPane newPanel;
+                    try {
+                        newPanel = FXMLLoader.load(getClass().getResource("/UiComponents/Fxml/userView.fxml"));
+                        contentPane.getChildren().setAll(newPanel);
+                    } catch (IOException ex) {
+                        Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    contentPane.setVisible(true);
+                
+                });
+
+            }else{
+                this.newNotification("Pin Incorrecto");
+            }
+        } catch (TwitterException ex) {
+            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    private void copyUrlThread(){
+        StringSelection stringSelection;
+        try {
+            stringSelection = new StringSelection(BOT.getInstance().generateUrl());
+            Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clpbrd.setContents(stringSelection, null);
+            Platform.runLater(()->{
+                this.newNotification("URL Copiado al portapapeles");
+            
+            });
+            
+        } catch (TwitterException ex) {
+            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }      
     
 }
