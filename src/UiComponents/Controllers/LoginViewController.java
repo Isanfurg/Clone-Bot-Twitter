8f270c64ca74333368bddfc794e701b5d9e089af
Isanfurg <sanfurgoh@gmail.com>
2019-09-27 16:45:04 -0300

    Revert "animation login"
    This reverts commit 7618e39e5c9aee0ce6b9d3cc3034a9de3a84b9fc.

--- src/UiComponents/Controllers/LoginViewController.java
+++ src/UiComponents/Controllers/LoginViewController.java
@@ -13,31 +13,13 @@ import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-<<<<<<< Updated upstream
-=======
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.animation.FadeTransition;
-import javafx.animation.RotateTransition;
-import javafx.application.Platform;
->>>>>>> Stashed changes
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-<<<<<<< Updated upstream
-=======
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.Duration;
->>>>>>> Stashed changes
 import twitter4j.TwitterException;
 
 /**
@@ -51,45 +33,24 @@ public class LoginViewController implements Initializable,Notification {
     private AnchorPane contentPane;
     @FXML
     private TextField pinBox;
-<<<<<<< Updated upstream
-=======
-    @FXML
-    private JFXSpinner loadSpinner;
-    @FXML
-    private Circle toRotate;
->>>>>>> Stashed changes
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        toRotate.setFill(new ImagePattern(new Image("/UiComponents/Img/outIcon.png")));
-        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(20),toRotate);
-        rotateTransition.setToAngle(360);
-        rotateTransition.setCycleCount(20);
-        rotateTransition.setRate(3);
-        rotateTransition.play();
-       
     }    
 
     @FXML
     private void copiarUrl(ActionEvent event) throws TwitterException {
-<<<<<<< Updated upstream
         StringSelection stringSelection = new StringSelection(BOT.getInstance().generateUrl());
         Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
         clpbrd.setContents(stringSelection, null);
         this.newNotification("URL Copiado al portapapeles");
-=======
-       
-       new Thread(this::copyUrlThread).start();
-
->>>>>>> Stashed changes
     }
 
     @FXML
     private void login(ActionEvent event) throws IOException, TwitterException {
-<<<<<<< Updated upstream
         BOT.getInstance().tryPin(pinBox.getText());
         if(BOT.getInstance().isAccess()){
             this.newNotification("Pin Correcto");
@@ -97,42 +58,6 @@ public class LoginViewController implements Initializable,Notification {
             contentPane.getChildren().setAll(newPanel);   
         }else{
             this.newNotification("Pin Incorrecto");
-=======
-        fadeContentPane(1, 0, 1000);
-        new Thread(this::loginThread).start();
-       
-        
-    }
-
-    private void loginThread(){
-        
-        
-        try {
-            BOT.getInstance().tryPin(pinBox.getText());
-            
-            if(BOT.getInstance().isAccess()){
-                
-                Platform.runLater(()->{
-                    
-                    StackPane newPanel;
-                    try {
-                        
-                        newPanel = FXMLLoader.load(getClass().getResource("/UiComponents/Fxml/userView.fxml"));
-                        contentPane.getChildren().setAll(newPanel);
-                    } catch (IOException ex) {
-                        Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    
-                    fadeContentPane(0, 1, 2000);
-                
-                });
-
-            }else{
-                this.newNotification("Pin Incorrecto");
-            }
-        } catch (TwitterException ex) {
-            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
->>>>>>> Stashed changes
         }
     }
     
